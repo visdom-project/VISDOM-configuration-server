fdfc06f04af1e6f079c6af0b7c266522c955ece0
Duc Hong <duc.hong@tuni.fi>
2021-08-25 15:38:48 +0300

    change to send error message to client

--- router.js
+++ router.js
@@ -8,7 +8,7 @@ router.post("/:visId", async (req, res) => {
     const { name, config } = req.body;
 
     if (!config || !name){
-        return res.status(400).json({error: "Not found parameters"});
+        return res.status(400).send({error: "Not found parameters"});
     }
 
     if (!vis) {
@@ -19,12 +19,12 @@ router.post("/:visId", async (req, res) => {
         }
         catch (err) {
             console.log(err);
-            return res.status(400).json({error: "bad microfrontend name"});
+            return res.status(400).send({error: "bad microfrontend name"});
         }
     }
     
     if (vis.configurations.filter(config => config.name === name).length !== 0) {
-        return res.status(400).json({error: "duplicate configuration name"});
+        return res.status(400).send({error: "duplicate configuration name"});
     }
     vis.configurations.push({name: name, config: config});
     await vis.save();
@@ -42,7 +42,7 @@ router.get("/:visId/", async (req, res) => {
     const configurations = vis.configurations;
     const config = configurations.filter(configuration => configuration.name === configName);
     if (config.length === 0){
-        return res.status(404).json({error: "config not found with name"});
+        return res.status(404).send({error: "config not found with name"});
     }
     return res.status(200).json(config[0])
 })
@@ -54,13 +54,13 @@ router.delete("/:visId", async (req, res) => {
     const x_key = req.get("x-api-key");
     const configName = req.get("config-name");
     if (x_key !== X_KEY){
-        return res.status(400).json({error: "No access right"});
+        return res.status(400).send({error: "No access right"});
     }
 
     const vis = await DB.findOne({microfrontendName: visId});
     
     if (!vis){
-        return res.status(400).json({error: "bad microfrontend name"});
+        return res.status(400).send({error: "bad microfrontend name"});
     }
 
     vis.configurations = vis.configurations.filter(config => config.name !== configName);
@@ -72,7 +72,7 @@ router.delete("/all/:visId", async (req, res) => {
     const x_key = req.get("x-api-key");
     
     if (x_key !== X_KEY){
-        return res.status(400).json({error: "No access right"});
+        return res.status(400).send({error: "No access right"});
     }
     const visId = req.params.visId;
     await DB.findOneAndDelete({microfrontendName: visId});
